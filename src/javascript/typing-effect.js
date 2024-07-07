1a67643b62468cf703eeca98cf024848625bbd82
jonathanspereira <jonathanpereira.jsp@outlook.com>
2024-07-06 22:01:11 -0300

    criacação: criação do sobre e funcionalidades, organização de arquivos e pastas // refatorar: remover código JavaScript e CSS não utilizados

--- src/javascript/typing-effect.js
+++ src/javascript/typing-effect.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["FRONT-END", "BACK-END", "DATA ANALYST"];
+const textArray = ["FRONT-END", "BACK-END", "AND DATA ANALYST"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
@@ -39,3 +39,7 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
   if(textArray.length) setTimeout(type, newTextDelay + 250);
 });
+
+module.exports = { type, erase };
+
+
